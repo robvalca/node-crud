ccb4cd4455df2b3a1fa00a4bccf4f2398a22efbc
Roberto Valverde <rvcameselle@esei.uvigo.es>
2019-01-05 20:08:09 +0100

    esqueleto

---
+++ src/app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+// Import routes
+
+// Settings
+
+// Middleware
+
+// Starting Server
+app.listen(3000, () => {
+   console.log('Servidor Escuchando en Puerto 3000')
+});
